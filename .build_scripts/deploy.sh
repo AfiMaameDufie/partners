db57f3fe8234af87011d23ecafe7dd635a72c396
AfiMaameDufie <afimaamedufie@gmail.com>
2023-03-15 19:58:09 +0000

    Removed square-braces off deploy branch specified

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -2,7 +2,7 @@
 set -e # halt script on error
 
 # If this is the deploy branch, push it up to gh-pages
-if [$CIRCLE_BRANCH = ${DEPLOY_BRANCH}] ; then
+if $CIRCLE_BRANCH = ${DEPLOY_BRANCH} ; then
   echo "Get ready, we're pushing to gh-pages!"
   git config user.name "CIRCLECI"
   git config user.email "circleci@somewhere.com"
