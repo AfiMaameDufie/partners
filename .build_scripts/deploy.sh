ab3b7da4c630076c00680c49e425f43f534da1d4
AfiMaameDufie <afimaamedufie@gmail.com>
2023-03-16 10:26:22 +0000

    Changed the branch

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -6,7 +6,7 @@ if $CIRCLE_BRANCH = "$DEPLOY_BRANCH" ; then
   echo "Get ready, we're pushing to gh-pages!"
   git config user.name "CIRCLECI"
   git config user.email "circleci@somewhere.com"
-  node_modules/.bin/gh-pages --silent --dist _site -branch fix/deploy_script --repository "https://${GH_TOKEN}github.com/${CIRCLE_REPOSITORY_URL}.git"
+  node_modules/.bin/gh-pages --silent --dist _site --branch fix/deploy_script --repository "https://${GH_TOKEN}github.com/${CIRCLE_REPOSITORY_URL}.git"
 else
   echo "Not a publishable branch so we're all done here"
 fi
