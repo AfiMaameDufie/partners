5cfdbaa3de81161cf8a61d5fcdd0ed7860006d7e
AfiMaameDufie <afimaamedufie@gmail.com>
2023-03-20 21:24:53 +0000

    Changing build and deploy command

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -5,10 +5,10 @@ set -e # halt script on error
 if [[ $CIRCLE_BRANCH = ${DEPLOY_BRANCH} ]]; then
   echo "CIRCLE_BRANCH: "$CIRCLE_BRANCH
   echo "\$DEPLOY_BRANCH: " ${DEPLOY_BRANCH}
-  echo "Get ready, we're pushing to gh-pages!"
+  echo "Get ready, we're pushing to branch!"
   git config user.name "CIRCLECI"
   git config user.email "circleci@somewhere.com"
-  node_modules/.bin/gh-pages --silent --dist _site --branch fix/deploy_script --repository "https://${GH_TOKEN}github.com/${CIRCLE_REPOSITORY_URL}.git"
+  node_modules/.bin/gh-pages --silent -d dist _site -b fix/deploy_script --repo ${CIRCLE_REPOSITORY_URL}
 else
   echo "Not a publishable branch so we're all done here"
 fi
