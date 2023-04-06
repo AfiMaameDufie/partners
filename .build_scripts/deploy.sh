c1a27361e17721ccc028e52cf2dff4e6ed6c89b2
AfiMaameDufie <afimaamedufie@gmail.com>
2023-04-06 11:38:03 +0000

    Publishing branch

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-# If this is the deploy branch, push it up to gh-pages
 if [[ $CIRCLE_BRANCH = ${DEPLOY_BRANCH} ]]; then
   echo "CIRCLE_BRANCH: " $CIRCLE_BRANCH
   echo "DEPLOY_BRANCH: " ${DEPLOY_BRANCH}
